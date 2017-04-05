13947555a501d5e6be322683cd1aeca4e664987b
Bartosz Michalak <bartosz.michalak@crusar.eu>
2017-04-05 15:56:11 +0200

    Add some tests and validate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,25 @@
 class ArticlesController < ApplicationController
-	def new
+	 def index
+      @articles = Article.all
+  end
+  def new
 	end
-
 	def create
   		@article = Article.new(article_params)
- 
-  		@article.save
-  		redirect_to @article
+  
+  		if @article.save
+  		  redirect_to @article
+      else
+        render 'new'
+      end
 	end
- 
-	private
-  		def article_params
-    	params.require(:article).permit(:title, :text)
-  	end
 
   	def show
   		@article = Article.find(params[:id])
   	end
+
+      private
+      def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
